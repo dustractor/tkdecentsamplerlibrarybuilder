9252e5fb0fc6694454dd47267a743d7db8fc9c90
Shams Kitz <dustractor@gmail.com>
2023-08-09 21:56:10 -0500

    Create batch_build_example2.cmd

---
+++ examples/batch_build_example2.cmd
@@ -0,0 +1,38 @@
+rem @echo off
+set BUILDER=%HOMEDRIVE%%HOMEPATH%\Documents\GitHub\tkdecentsamplerlibrarybuilder\tkdecentsamplerlibrarybuilder.py
+set PATH=%HOMEDRIVE%%HOMEPATH%\ANACON~1;%PATH%
+set PYTHONHOME=%HOMEDRIVE%%HOMEPATH%\anaconda3
+set PY=%PYTHONHOME%\pythonw.exe
+call conda activate
+
+
+%PY% %BUILDER% --do-build --input-folder .\inputdir\100 --output-folder .\outputdir\DS_hhloop_100 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\110 --output-folder .\outputdir\DS_hhloop_110 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\118 --output-folder .\outputdir\DS_hhloop_118 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\120 --output-folder .\outputdir\DS_hhloop_120 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\124 --output-folder .\outputdir\DS_hhloop_124 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\128 --output-folder .\outputdir\DS_hhloop_128 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\130 --output-folder .\outputdir\DS_hhloop_130 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\133 --output-folder .\outputdir\DS_hhloop_133 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\136 --output-folder .\outputdir\DS_hhloop_136 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\140 --output-folder .\outputdir\DS_hhloop_140 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\144 --output-folder .\outputdir\DS_hhloop_144 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\150 --output-folder .\outputdir\DS_hhloop_150 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\155 --output-folder .\outputdir\DS_hhloop_155 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\160 --output-folder .\outputdir\DS_hhloop_160 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\166 --output-folder .\outputdir\DS_hhloop_166 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\170 --output-folder .\outputdir\DS_hhloop_170 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\174 --output-folder .\outputdir\DS_hhloop_174 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\175 --output-folder .\outputdir\DS_hhloop_175 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\176 --output-folder .\outputdir\DS_hhloop_176 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\180 --output-folder .\outputdir\DS_hhloop_180 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\200 --output-folder .\outputdir\DS_hhloop_200 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\210 --output-folder .\outputdir\DS_hhloop_210 --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\80  --output-folder .\outputdir\DS_hhloop_80  --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\88  --output-folder .\outputdir\DS_hhloop_88  --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\90  --output-folder .\outputdir\DS_hhloop_90  --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\92  --output-folder .\outputdir\DS_hhloop_92  --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+%PY% %BUILDER% --do-build --input-folder .\inputdir\96  --output-folder .\outputdir\DS_hhloop_96  --start-note 0 --cut-all-by-all --silencing-mode fast --have-chorus --have-tone --have-midicc1
+
+
+
